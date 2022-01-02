af47ee6d51f527af32df69b52c1380ce492fd5d1
Pratik_Tulkane <pratiktulkane12345@gmail.com>
2022-01-02 10:55:04 +0530

    Updating file

--- src/Component.js/News.js
+++ src/Component.js/News.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export default class News extends Component {
+
+    let 
+    constructor(){
+        super();
+        this.state ={
+
+        }
+    }
     
-    // constructor()
-    //     super();
-    //     this.state ={
-    //         articles: this.articles
-    //     }
     render() {
         return (
             <div className='container my-3'>
                 <h1>Top HeadLines</h1>
                 <div className="row">
+                <div className="col-md-4">
+                    <NewsItem title="Mytitle" description="Mydesc" imageUrl="https://www.gannett-cdn.com/presto/2021/12/31/USAT/440eafaa-f619-4157-9dfe-36607524ee60-AP_APTOPIX_Colorado_Wildfires.jpg?crop=4972,2797,x0,y110&width=3200&height=1801&format=pjpg&auto=webp"/>
+                </div>
                 <div className="col-md-4">
                     <NewsItem title="Mytitle" description="Mydesc" imageUrl="https://www.gannett-cdn.com/presto/2021/12/31/USAT/440eafaa-f619-4157-9dfe-36607524ee60-AP_APTOPIX_Colorado_Wildfires.jpg?crop=4972,2797,x0,y110&width=3200&height=1801&format=pjpg&auto=webp"/>
                 </div>
