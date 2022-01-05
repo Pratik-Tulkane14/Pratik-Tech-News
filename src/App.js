c39ca8c8f7d76043dd2b76dcddb8f86807d42331
Pratik_Tulkane <pratiktulkane12345@gmail.com>
2022-01-05 14:03:42 +0530

    Empty-Routes-Working

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React, { Component } from 'react'
 import Navbar from './Component.js/Navbar';
 import News from './Component.js/News';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route ,
+} from "react-router-dom";
+
 export default class App extends Component {
  
   render() {
     return (
+      <>
       <div>
+        <Router>
         <Navbar/>
-        <News pageSize={6} country={"in"} category="science"/>
+        <Routes>
+          <Route path="/" element={<News pageSize={6} country="in" category="general" />}></Route>
+          {/* <Route path="/business"><News pageSize={6} country="in" category="business"/></Route>
+          <Route path="/entertainment"><News pageSize={6} country="in" category="entertainment"/></Route>
+          <Route path="/general"><News pageSize={6} country="in" category="general"/></Route>
+          <Route path="/health"><News pageSize={6} country="in" category="health"/></Route>
+          <Route path="/science"><News pageSize={6} country="in" category="science"/></Route>
+          <Route path="/sports"><News pageSize={6} country="in" category="sports"/></Route>
+          <Route path="/technology"><News pageSize={6} country="in" category="technology"/></Route> */}
+        </Routes>
+        </Router>
       </div>
+      </>
     )
   }
 }
