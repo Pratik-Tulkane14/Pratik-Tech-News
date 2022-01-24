fcbeaafc8fefae98c0e5b7b502575b3dffd24f49
Pratik_Tulkane <pratiktulkane12345@gmail.com>
2022-01-24 09:33:27 +0530

    testing

--- src/Component.js/News.js
+++ src/Component.js/News.js
@@ -55,14 +55,13 @@ const News = (props)=>{
                     loader={<Spinner/>}
                 > 
                     <div className="container">
-                         
-                    <div className="row">
-                        {articles.map((element) => {
-                            return <div className="col-md-4" key={element.url}>
-                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-                            </div>
-                        })}
-                    </div>
+                        <div className="row">
+                            {articles.map((element) => {
+                                return <div className="col-md-4" key={element.url}>
+                                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                                        </div>
+                            })}
+                        </div>
                     </div> 
                 </InfiniteScroll>
             </>
